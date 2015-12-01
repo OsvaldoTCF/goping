a72db47e079006b50e71c51929b96d413269fdee
Anthony Seure <anthony.seure@gmail.com>
2015-12-01 23:54:31 +0100

    Rename HTTP request handler functions.

--- goping.go
+++ goping.go
@@ -17,16 +17,16 @@ func main() {
 
 	// Instantiates and configures the HTTP router
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/api/1/pings", PingAdd).Methods("POST")
-	router.HandleFunc("/api/1/pings/{origin}/hours", PingGetAvgPerHour).Methods("GET")
-	router.HandleFunc("/", PingChartWebPage).Methods("GET")
+	router.HandleFunc("/api/1/pings", HandlerAdd).Methods("POST")
+	router.HandleFunc("/api/1/pings/{origin}/hours", HandlerGetAvgPerHour).Methods("GET")
+	router.HandleFunc("/", HandlerChartWebPage).Methods("GET")
 
 	// Start the server
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 // Handler of /api/1/pings POST requests to add new datapoints in the TSDB.
-func PingAdd(w http.ResponseWriter, r *http.Request) {
+func HandlerAdd(w http.ResponseWriter, r *http.Request) {
 	pings, err := utils_json.ReadPings(r)
 	if err != nil {
 		log.Println(err)
@@ -37,7 +37,7 @@ func PingAdd(w http.ResponseWriter, r *http.Request) {
 
 // Handler of /api/1/pings/{origin}/hours GET requests to retrieve the everage
 // `transfer_time_ms` for a specific `origin`, aggregated by hours.
-func PingGetAvgPerHour(w http.ResponseWriter, r *http.Request) {
+func HandlerGetAvgPerHour(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	origin := vars["origin"]
 	averages := connector.GetAveragePerHour(origin)
@@ -47,5 +47,5 @@ func PingGetAvgPerHour(w http.ResponseWriter, r *http.Request) {
 
 // Handler of / GET requests to display a chart of the `transfer_time_ms`
 // aggregated by hours with a dropdown menu to change the `origin`.
-func PingChartWebPage(w http.ResponseWriter, r *http.Request) {
+func HandlerChartWebPage(w http.ResponseWriter, r *http.Request) {
 }
